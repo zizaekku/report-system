c5582bbfd6cab519f555357f211f6f6b752e9426
JiwooAn <jiyouan9@gmail.com>
2022-10-20 10:35:37 +0900

    xlsx file upload

--- report/urls.py
+++ report/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import *
+
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('upload/', include('upload.urls')),
 ]
